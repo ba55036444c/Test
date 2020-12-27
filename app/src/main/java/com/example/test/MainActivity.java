e2411df4143977ee4185241b4325a449e26c40ca
文啓金 <13767486553@163.com>
2020-12-27 17:00:14 +0800

    测试

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"sss",Toast.LENGTH_LONG).show();
+        //在dev分支上修改内容
     }
 }
