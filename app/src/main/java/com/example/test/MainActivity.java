f9895b174281822b449852e3c43018f97f4fe067
文啓金 <13767486553@163.com>
2021-01-12 14:43:59 +0800

    修改TestClass2，添加TestClass4

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
+
+import com.example.test.Drag.ChannelActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"sss",Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "sss", Toast.LENGTH_LONG).show();
         //在dev分支上修改内容
         //在dev分支上修改内容2222
+        findViewById(R.id.tv_device_name2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ChannelActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/test/TestClass4.java
+++
@@ -1,7 +0,0 @@
-package com.example.test;
-
-public class TestClass4 {
-    private int a = 0;
-    private int a2 = 0;
-    private int a3 = 0;
-}
